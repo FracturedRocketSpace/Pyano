5c2486fd100771b37f30049cff36cb8231fdcdf7
mgarming <mgarming@outlook.com>
2016-04-25 13:55:26 +0200

    Nan

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
 import math
 
-tention = 750                       #String tention [N]
+tension = 750                       #String tension [N]
 elasticModulus = 2e11               #Young's modulus [N/m^2]
 diameter = 1e-3                     #String diameter [m]
 crossArea = math.pi/4*diameter**2   #Cross sectional area of a string [m^2]
 gyration = diameter/4               #Radius of gyration for a cilindrical string is half the radius [m]
-density = 0.018                     #linear density [kg/m]
+density = 8050*crossArea                   #linear density [kg/m]
 
 length = 1                          #String length [m]
+dx = 0.001                          #x element size (m)
+
+tmax = 0.5
+dt = 0.01
+
+hammerSize = 0.02                   #Length of string hit by hammer
+hammerVelocity = 1                  #Initial velocity of string due to hammer
+hammerLocation = 0.15               #Distance from the end of the string to hammer striking location
--- main.py
+++ main.py
@@ -1,20 +1,75 @@
 import numpy as np
 import math
 import config as c
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D # Required for projection='3d'!
+from matplotlib import cm
+from matplotlib import animation
+
 
 #theoretical values
-speed = np.sqrt(c.tention/c.density)
+speed = np.sqrt(c.tension/c.density)
 f0 = speed/(2*c.length);
-inharmonicity = math.pi**2*c.elasticModulus*c.crossArea*c.gyration**2/(c.tention*c.length**2);
+inharmonicity = math.pi**2*c.elasticModulus*c.crossArea*c.gyration**2/(c.tension*c.length**2);
 
 #inharmonic partial frequencies
-n = 1;
-fn = n*f0*np.sqrt(1+inharmonicity*n**2);
+order = 1;
+fn = order*f0*np.sqrt(1+inharmonicity*order**2);
 
+x = np.arange(0, c.length + c.dx, c.dx)
+t = np.arange(0, c.tmax + c.dt, c.dt)
 
-#simulate strings
+#Initiate arrays for deviation and velcity
+dev = np.zeros([len(x),len(t)])
+vel = np.zeros([len(x),len(t)])
+
+# Initial conditino: Strike string with hammer
+vel[int(c.hammerLocation/c.length*len(x) ): int((c.hammerLocation+c.hammerSize)/c.length*len(x)), 0] = c.hammerVelocity;
+
+def calcForce(dev):
+    devPadded = np.zeros([len(x)+4])
+    devPadded[2:-2]=dev
+
+    force1 = c.tension/c.dx**2* (devPadded[1:-3] - 2*devPadded[2:-2] + devPadded[3:-1] ) 
+    force2 = c.elasticModulus*c.crossArea*c.gyration**2/c.dx**4 * (devPadded[0:-4] - 4*devPadded[1:-3] + 6* devPadded[2:-2] - 4*devPadded[3:-1] + devPadded[4::])   
+    force = force1+force2  
+    print( np.isnan(force1).any() )
+    print( np.isnan(force2).any() )
+    return force, force1, force2
+
+def iterate(dev, vel, force):
+    
+    accelleration = force/ (c.density*c.dx)
+
+    devNew = dev + vel*c.dt + 1/2*accelleration*c.dt**2;
+    velNew = vel + 1/2*accelleration*c.dt
+    
+    force, force1, force2 = calcForce(devNew)
+    
+    velNew += 1/2 * force/(c.density*c.dx) * c.dt
+        
+    return devNew, velNew, force, force1, force2
+    
+# force for first iteration
+force, force1, force2 = calcForce(dev[:,0])
+for i in range(1, len(t)):
+    dev[:,i], vel[:,i], force, force1, force2 = iterate(dev[:,i-1], vel[:,i-1], force)
 
 #plot/animate results: string animation, frequency spectrum
+X, T =np.meshgrid(x,t)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf = ax.plot_surface(X,T ,dev.T, rstride=10, cstride=10,cmap=cm.coolwarm, linewidth=0, antialiased=False)
+ax.view_init(90, 90); # Top view
+plt.xlabel("Position")
+plt.ylabel("Time")
+plt.title("Implicit method")
+fig.colorbar(surf, shrink=0.5, aspect=5)
+# Hide z-axis
+ax.w_zaxis.line.set_lw(0.)
+ax.set_zticks([])
+    
 
 #output sounds
 
