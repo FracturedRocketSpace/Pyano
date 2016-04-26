bb7013bb32a79655385618548ff17041aad985a4
Fabian Fool <fool0003@hotmail.com>
2016-04-26 11:19:52 +0200

    Code cleanup and fixes

--- config.py
+++ config.py
@@ -1,25 +1,24 @@
 import math
 
-tension = 750                       #String tension [N]
-density =0.460/0.09;
-c = (tension/density)**(1/2)
-eps = 8.67e-4;
-b1 = 0.5;
-b3 = 2.6e-10;
+tension = 750  # String tension [N]
+density = 0.460 / 0.09;  # Densiuty [kg/m]
+c = (tension / density) ** (1 / 2)  # Speed of sound
+eps = 8.67e-4;  # Stiffness
+b1 = 0.5;  # First damping
+b3 = 2.6e-10;  # Third damping
 
+length = 0.09;  # String length [m]
+dx = 0.09 / 64;  # x element size (m)
 
-length = 0.09;                         #String length [m]
-dx = 0.09/64;                        #x element size (m)
+tmax = 5  # Simulated time [s]
+dt = 1 / (96e3);  # Time step [s]
 
-tmax = 1
-dt = 1/(96e3);
+hammerSize = 0.0054 / 2  # Length of string hit by hammer
+hammerVelocity = 1  # Initial velocity of string due to hammer
+hammerLocation = 0.0054  # Distance from the end of the string to hammer striking location
 
-hammerSize = 0.0054/2               #Length of string hit by hammer
-hammerVelocity = 1                  #Initial velocity of string due to hammer
-hammerLocation = 0.0054             #Distance from the end of the string to hammer striking location
-
-bridgePos = hammerLocation;
-format = 1; # DUnno what this means
-CHUNK = 5*1024 # CHUNK load size
-numChannels = 1; # = Mono
-framerate = int(1/dt);
+bridgePos = hammerLocation / 2;
+format = 1;  # DUnno what this means
+CHUNK = 5 * 1024  # CHUNK load size
+numChannels = 1;  # = Mono
+framerate = int(1 / dt);
--- main.py
+++ main.py
@@ -2,108 +2,84 @@ import numpy as np
 import math
 import config as c
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D # Required for projection='3d'!
+from mpl_toolkits.mplot3d import Axes3D  # Required for projection='3d'!
 from matplotlib import cm
 from matplotlib import animation
-import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.mlab as mlab
 import math
-import scipy
 import scipy.sparse
 import timeit
 import pyaudio
 
-
-#theoretical values
-#speed = np.sqrt(c.tension/c.density)
-#f0 = speed/(2*c.length);
-#inharmonicity = math.pi**2*c.elasticModulus*c.crossArea*c.gyration**2/(c.tension*c.length**2);
-
-#inharmonic partial frequencies
-#order = 1;
-#fn = order*f0*np.sqrt(1+inharmonicity*order**2);
-
-x = np.arange(-c.dx, c.length + 2*c.dx, c.dx)
+x = np.arange(-c.dx, c.length + 2 * c.dx, c.dx)
 t = np.arange(0, c.tmax + c.dt, c.dt)
 
-#Initiate arrays for deviation and velcity
-dev = np.zeros([len(x),len(t)])
-vel = np.zeros([len(x),len(t)])
+# Initiate arrays for deviation and velocity
+dev = np.zeros([len(x), len(t)])
+vel = np.zeros([len(x), len(t)])
 
 # Initial conditino: Strike string with hammer
-
-
+# Gaussian hammer
 mean = c.hammerLocation;
 variance = c.hammerSize;
 sigma = math.sqrt(variance);
-#vel[:,0] = c.hammerVelocity * mlab.normpdf(x,mean,sigma);
-
-vel[int(c.hammerLocation/c.length*len(x) ): int((c.hammerLocation+c.hammerSize)/c.length*len(x)), 0] = c.hammerVelocity;
+vel[:, 0] = c.hammerVelocity * mlab.normpdf(x, mean, sigma);
+# Pulse hammer
+# vel[int(c.hammerLocation/c.length*len(x) ): int((c.hammerLocation+c.hammerSize)/c.length*len(x)), 0] = c.hammerVelocity;
 
 # force for first iteration
-dev[:,2] += vel[:,0]*c.dt
-#force = calcForce(dev[:,0])
-#dev[:,0] += 0.5*force*c.dt**2 /(c.density*c.dx)
+dev[:, 0] += vel[:, 0] * c.dt
+dev[:, 1] += vel[:, 0] * c.dt
+dev[:, 2] += vel[:, 0] * c.dt
 
 # Create matrices
-D = 1 + c.b1*c.dt+2*c.b3/c.dt;
+D = 1 + c.b1 * c.dt + 2 * c.b3 / c.dt;
 r = c.c * c.dt / c.dx;
 N = len(x)
 
-a1 = (2 -2*r**2 + c.b3/c.dt - 6*c.eps*(N**2)*(r**2) ) / D;
-a2 = ( -1 + c.b1*c.dt + 2*c.b3/c.dt ) / D;
-a3= ( r**2*(1+4*c.eps*(N**2) ) ) / D;
-a4 = (c.b3/c.dt - c.eps*(N**2)*(r**2) ) / D;
-a5= (- c.b3/c.dt ) / D;
+a1 = (2 - 2 * r ** 2 + c.b3 / c.dt - 6 * c.eps * (N ** 2) * (r ** 2)) / D;
+a2 = (-1 + c.b1 * c.dt + 2 * c.b3 / c.dt) / D;
+a3 = (r ** 2 * (1 + 4 * c.eps * (N ** 2))) / D;
+a4 = (c.b3 / c.dt - c.eps * (N ** 2) * (r ** 2)) / D;
+a5 = (- c.b3 / c.dt) / D;
 # Padded!
-diagonals1 =  [a1*np.ones(N), a3*np.ones(N-1), a3*np.ones(N-1), a4*np.ones(N-2), a4*np.ones(N-2) ]
+diagonals1 = [a1 * np.ones(N), a3 * np.ones(N - 1), a3 * np.ones(N - 1), a4 * np.ones(N - 2), a4 * np.ones(N - 2)]
 diagonals1[0][0] = diagonals1[0][-1] = 0;
 diagonals1[1][0] = diagonals1[2][-1] = 0;
 diagonals1[3][0] = diagonals1[4][-1] = 0;
-A1 = scipy.sparse.diags(diagonals1, [0,1,-1, 2, -2], format="csc")
+A1 = scipy.sparse.diags(diagonals1, [0, 1, -1, 2, -2], format="csr")
 
-diagonals2 = [a2*np.ones(N), a5*np.ones(N-1), a5*np.ones(N-1)];
+diagonals2 = [a2 * np.ones(N), a5 * np.ones(N - 1), a5 * np.ones(N - 1)];
 diagonals2[0][0] = diagonals2[0][-1] = 0;
 diagonals2[1][0] = diagonals2[2][-1] = 0;
-A2 = scipy.sparse.diags(diagonals2, [0,1,-1], format="csc");
+A2 = scipy.sparse.diags(diagonals2, [0, 1, -1], format="csr");
 
-diagonals3 = [a5*np.ones(N)]
+diagonals3 = [a5 * np.ones(N)]
 diagonals3[0][0] = diagonals3[0][-1] = 0;
-A3 = scipy.sparse.diags(diagonals3, [0], format="csc");
-
-# Process boundary conditions
-# End are 0
-A1[0,1] = A1[0,2] = 0;
-A1[0,-2] = A1[0,-3] = 0;
-A2[0,1] = A2[0,-2] = 0;
-# 2nd derivative 0
-#a3_new = a3= ( r**2*(4*c.eps*(N**2) ) ) / D;
-
-
-
+A3 = scipy.sparse.diags(diagonals3, [0], format="csr");
 
 # THE ITERATORN MWHAUHAHAHAH
 start = timeit.default_timer()
 for i in range(3, len(t)):
-    dev[:,i] = A1.dot(dev[:,i-1]) + A2.dot(dev[:,i-2]) + A3.dot(dev[:,i-3]);
+    dev[:, i] = A1.dot(dev[:, i - 1]) + A2.dot(dev[:, i - 2]) + A3.dot(dev[:, i - 3]);
     # end zero
-    dev[1,i] = 0;
+    dev[1, i] = 0;
     dev[-2, i] = 0;
     # 2nd
-    dev[0,i] = -dev[2,i];
+    dev[0, i] = -dev[2, i];
     dev[-1, i] = -dev[-3, i];
-    print('Now at ', i+1 ,'of the ', len(t));
+    print('Now at ', i + 1, 'of the ', len(t));
 print("Program ended in  =", int(timeit.default_timer() - start), "seconds");
 
 # Get sound output
-audio = dev[int(c.bridgePos/c.length*len(x) ),:];
+audio = dev[int(c.bridgePos / c.length * len(x)), :];
 print(len(audio))
 # Normalize and convert
 norm = max(abs(audio));
-audio = audio/norm;
-audio_out = np.array(audio*127 + 128, dtype=np.int8).view('c');
+audio = audio / norm;
+audio_out = np.array(audio * 127 + 128, dtype=np.int8).view('c');
 # Init sound
 p = pyaudio.PyAudio()
 # Open stream to audio device
@@ -116,9 +92,9 @@ stream = p.open(format=p.get_format_from_width(c.format),
                 rate=c.framerate,
                 output=True)
 
-#output sounds
+# output sounds
 start = timeit.default_timer()
-stream.write(audio)
+stream.write(audio_out)
 print("Program ended in  =", int(timeit.default_timer() - start), "seconds");
 
 # Stop the audio output
@@ -127,9 +103,8 @@ stream.close()
 
 p.terminate();
 
-
-#plot/animate results: string animation, frequency spectrum
-X, T =np.meshgrid(x,t)
+# plot/animate results: string animation, frequency spectrum
+X, T = np.meshgrid(x, t)
 
 # fig = plt.figure()
 # ax = fig.gca(projection='3d')
@@ -140,10 +115,10 @@ X, T =np.meshgrid(x,t)
 # plt.title("Implicit method")
 # fig.colorbar(surf, shrink=0.5, aspect=5)
 # Hide z-axis
-ax.w_zaxis.line.set_lw(0.)
-ax.set_zticks([])
+# ax.w_zaxis.line.set_lw(0.)
+# ax.set_zticks([])
 
 plt.figure()
-plt.plot(t,audio)
+plt.plot(t, audio * 127)
 
 plt.show()
