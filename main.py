089bc847ab9f04b74f39b3ab71147eae895d9ed3
mgarming <mgarming@outlook.com>
2016-04-27 13:13:24 +0200

    Bind buttons to playback

--- main.py
+++ main.py
@@ -17,141 +17,143 @@ import pyaudio
 from numba import jit
 from config import selectParameters
 
-
-note = input("input note")
-length, tension, b1, b3, hammerExponent, hammerLocation, hammerMass, hammerStiffness, hammerSize, hammerVelocity, dx, tmax, Fs, dt, density, eps, vel = selectParameters(int(note))  
-
-x = np.arange(-dx, length + 2 * dx, dx)
-t = np.arange(0, tmax + dt, dt)
-
-# Initiate arrays for deviation and velocity
-dev = np.zeros([len(x), len(t)])
-
-## Initial conditino: Strike string with hammer
-## Gaussian hammer
-#mean = c.hammerLocation;
-#variance = c.hammerSize;
-#sigma = math.sqrt(variance);
-#vel[:, 0] = c.hammerVelocity * mlab.normpdf(x, mean, sigma);
-## Pulse hammer
-## vel[int(c.hammerLocation/c.length*len(x) ): int((c.hammerLocation+c.hammerSize)/c.length*len(x)), 0] = c.hammerVelocity;
-#
-## force for first iteration
-#dev[:, 0] += vel[:, 0] * c.dt
-#dev[:, 1] += vel[:, 0] * c.dt
-#dev[:, 2] += vel[:, 0] * c.dt
-
-# Create matrices
-D = 1 + b1 * dt + 2 * b3 / dt;
-r = vel * dt / dx;
-N = len(x)
-
-a1 = (2 - 2 * r ** 2 + b3 / dt - 6 * eps * (N ** 2) * (r ** 2)) / D;
-a2 = (-1 + b1 * dt + 2 * b3 / dt) / D;
-a3 = (r ** 2 * (1 + 4 * eps * (N ** 2))) / D;
-a4 = (b3 / dt - eps * (N ** 2) * (r ** 2)) / D;
-a5 = (- b3 / dt) / D;
-
-# Define spatial extent of hammer
-g = np.exp(- (x-hammerLocation*length)**2/ (2*hammerSize**2))
-# Initiate hammer variables
-hammerInteraction = True
-hammerDisplacement = np.zeros([len(t)+1])
-hammerDisplacement[3] = hammerVelocity*dt
-dev[:, 0] += g* hammerVelocity * dt
-dev[:, 1] += g* hammerVelocity * dt
-dev[:, 2] += g* hammerVelocity * dt
-
-A1 = np.zeros((N,N));
-A2 = np.zeros((N,N));
-A3 = np.zeros((N,N));
-i,j = np.indices(A1.shape);
-
-A1[i==j] = a1;
-A1[i==j-1] = A1[i==j+1] = a3;
-A1[i==j-2] = A1[i==j+2] = a4;
-A1[0,:] = A1[-1,:] = 0;
-
-A2[i==j] = a2;
-A2[i==j-1] = A2[i==j+1] = a5;
-A2[0,:] = A2[-1,:] = 0;
-
-A3[i==j] = a5;
-A3[0,:] = A3[-1,:] = 0;
-
-@jit( nopython=True )
-def iterate(dev1,dev2,dev3,A1,A2,A3):
-    dev = np.dot(A1,dev1) + np.dot(A2,dev2) + np.dot(A3,dev3);
-    # end zero
-    dev[1] = 0;
-    dev[-2] = 0;
-    # 2nd
-    dev[0] = -dev[2];
-    dev[-1] = -dev[-3];    
-    return dev
-
-# Running the simulation
-start = timeit.default_timer()
-for i in range(3, len(t)):
-    if hammerInteraction:       
-        hammerForce = hammerStiffness* abs(hammerDisplacement[i] - dev[int(hammerLocation*len(x)),i])**hammerExponent
-        hammerDisplacement[i+1]=2*hammerDisplacement[i]-hammerDisplacement[i-1]-(dt**2*hammerForce)/hammerMass
-
-        dev[:,i] += dt**2*len(x)*hammerForce*g/(density*length)
-        
-        if (hammerDisplacement[i]<dev[int(hammerLocation*len(x)),i]):
-            hammerInteraction = False
-            
-    dev[:, i] = iterate(dev[:, i - 1],dev[:, i - 2],dev[:, i - 3], A1, A2, A3);
-    if(i%1000 == 0):
-        print('Now at ', i + 1, 'of the ', len(t));
+def simulate(note):
+    #note = input("input note")
+    length, tension, b1, b3, hammerExponent, hammerLocation, hammerMass, hammerStiffness, hammerSize, hammerVelocity, dx, tmax, Fs, dt, density, eps, vel = selectParameters(int(note))  
     
-print("Program ended in  =", int(timeit.default_timer() - start), "seconds");
+    x = np.arange(-dx, length + 2 * dx, dx)
+    t = np.arange(0, tmax + dt, dt)
+    
+    # Initiate arrays for deviation and velocity
+    dev = np.zeros([len(x), len(t)])
+    
+    ## Initial conditino: Strike string with hammer
+    ## Gaussian hammer
+    #mean = c.hammerLocation;
+    #variance = c.hammerSize;
+    #sigma = math.sqrt(variance);
+    #vel[:, 0] = c.hammerVelocity * mlab.normpdf(x, mean, sigma);
+    ## Pulse hammer
+    ## vel[int(c.hammerLocation/c.length*len(x) ): int((c.hammerLocation+c.hammerSize)/c.length*len(x)), 0] = c.hammerVelocity;
+    #
+    ## force for first iteration
+    #dev[:, 0] += vel[:, 0] * c.dt
+    #dev[:, 1] += vel[:, 0] * c.dt
+    #dev[:, 2] += vel[:, 0] * c.dt
+    
+    # Create matrices
+    D = 1 + b1 * dt + 2 * b3 / dt;
+    r = vel * dt / dx;
+    N = len(x)
+    
+    a1 = (2 - 2 * r ** 2 + b3 / dt - 6 * eps * (N ** 2) * (r ** 2)) / D;
+    a2 = (-1 + b1 * dt + 2 * b3 / dt) / D;
+    a3 = (r ** 2 * (1 + 4 * eps * (N ** 2))) / D;
+    a4 = (b3 / dt - eps * (N ** 2) * (r ** 2)) / D;
+    a5 = (- b3 / dt) / D;
+    
+    # Define spatial extent of hammer
+    g = np.exp(- (x-hammerLocation*length)**2/ (2*hammerSize**2))
+    g[0]=0; g[-1]=0;
+    # Initiate hammer variables
+    hammerInteraction = True
+    hammerDisplacement = np.zeros([len(t)+1])
+    hammerDisplacement[3] = hammerVelocity*dt
+    dev[:, 0] += g* hammerVelocity * dt
+    dev[:, 1] += g* hammerVelocity * dt
+    dev[:, 2] += g* hammerVelocity * dt
+    
+    A1 = np.zeros((N,N));
+    A2 = np.zeros((N,N));
+    A3 = np.zeros((N,N));
+    i,j = np.indices(A1.shape);
+    
+    A1[i==j] = a1;
+    A1[i==j-1] = A1[i==j+1] = a3;
+    A1[i==j-2] = A1[i==j+2] = a4;
+    A1[0,:] = A1[-1,:] = 0;
+    
+    A2[i==j] = a2;
+    A2[i==j-1] = A2[i==j+1] = a5;
+    A2[0,:] = A2[-1,:] = 0;
+    
+    A3[i==j] = a5;
+    A3[0,:] = A3[-1,:] = 0;
+    
+    @jit( nopython=True )
+    def iterate(dev1,dev2,dev3,A1,A2,A3):
+        dev = np.dot(A1,dev1) + np.dot(A2,dev2) + np.dot(A3,dev3);
+        # end zero
+        dev[1] = 0;
+        dev[-2] = 0;
+        # 2nd
+        dev[0] = -dev[2];
+        dev[-1] = -dev[-3];    
+        return dev
+    
+    # Running the simulation
+    start = timeit.default_timer()
+    for i in range(3, len(t)):
+        if hammerInteraction:       
+            hammerForce = hammerStiffness* abs(hammerDisplacement[i] - dev[int(hammerLocation*len(x)),i])**hammerExponent
+            hammerDisplacement[i+1]=2*hammerDisplacement[i]-hammerDisplacement[i-1]-(dt**2*hammerForce)/hammerMass
+    
+            dev[:,i] += dt**2*len(x)*hammerForce*g/(density*length)
+            
+            if (hammerDisplacement[i]<dev[int(hammerLocation*len(x)),i]):
+                hammerInteraction = False
+                
+        dev[:, i] = iterate(dev[:, i - 1],dev[:, i - 2],dev[:, i - 3], A1, A2, A3);
+        if(i%1000 == 0):
+            print('Now at ', i + 1, 'of the ', len(t));
+        
+    print("Program ended in  =", int(timeit.default_timer() - start), "seconds");
+    
+    # Get sound output
+    bridgePos=.5
+    audio = dev[int(bridgePos * len(x) ), :];
+    print(len(audio))
+    # Normalize and convert
+    norm = max(abs(audio));
+    audio = audio / norm;
+    audio_out = np.array(audio * 127 + 128, dtype=np.int8).view('c');
+    # Init sound
+    p = pyaudio.PyAudio()
+    # Open stream to audio device
+    # Format: Array type. Int32 or float32 for example. 1 = float32?
+    # Channels. Number of channels. 1=mono, 2=stereo
+    # Rate: The sampling rate
+    # Output: True of course as we want output
+    stream = p.open(format=p.get_format_from_width(c.format),
+                    channels=c.numChannels,
+                    rate=int(Fs),
+                    output=True)
+    
+    # output sounds
+    start = timeit.default_timer()
+    stream.write(audio_out)
+    print("Program ended in  =", int(timeit.default_timer() - start), "seconds", flush=True);
+    
+    # Stop the audio output
+    stream.stop_stream()
+    stream.close()
+    p.terminate();
 
-# Get sound output
-bridgePos=.5
-audio = dev[int(bridgePos * len(x) ), :];
-print(len(audio))
-# Normalize and convert
-norm = max(abs(audio));
-audio = audio / norm;
-audio_out = np.array(audio * 127 + 128, dtype=np.int8).view('c');
-# Init sound
-p = pyaudio.PyAudio()
-# Open stream to audio device
-# Format: Array type. Int32 or float32 for example. 1 = float32?
-# Channels. Number of channels. 1=mono, 2=stereo
-# Rate: The sampling rate
-# Output: True of course as we want output
-stream = p.open(format=p.get_format_from_width(c.format),
-                channels=c.numChannels,
-                rate=int(Fs),
-                output=True)
-
-# output sounds
-start = timeit.default_timer()
-stream.write(audio_out)
-print("Program ended in  =", int(timeit.default_timer() - start), "seconds", flush=True);
-
-# Stop the audio output
-stream.stop_stream()
-stream.close()
-
-
-print("Calculating and plotting spectrum", flush=True)
-spectrum = scipy.fftpack.fft(audio)
-#spectrum = scipy.fftpack.fft(np.sin(2*np.pi*1000*t))
-freq= np.linspace(0,1/(2*dt),len(t)/2)
-plt.figure()
-plt.plot(freq, np.abs(spectrum[:len(t)/2]))
-
-plt.xlim(20,10000)
-plt.xlabel("Frequency (Hz)")
-plt.ylabel("Intensity (a.u.)")
-p.terminate();
+def plotSpectrum(audio, dt, t):
+    print("Calculating and plotting spectrum", flush=True)
+    spectrum = scipy.fftpack.fft(audio)
+    #spectrum = scipy.fftpack.fft(np.sin(2*np.pi*1000*t))
+    freq= np.linspace(0,1/(2*dt),len(t)/2)
+    plt.figure()
+    plt.plot(freq, np.abs(spectrum[:len(t)/2]))
+    
+    plt.xlim(20,10000)
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Intensity (a.u.)")
+    
 
 # plot/animate results: string animation, frequency spectrum
-X, T = np.meshgrid(x, t)
+#X, T = np.meshgrid(x, t)
 
 # fig = plt.figure()
 # ax = fig.gca(projection='3d')
@@ -165,9 +167,9 @@ X, T = np.meshgrid(x, t)
 # ax.w_zaxis.line.set_lw(0.)
 # ax.set_zticks([])
 
-plt.figure()
-plt.plot(t, audio * 127)
-plt.show()
+#plt.figure()
+#plt.plot(t, audio * 127)
+#plt.show()
 
 #UI for playing music
 whiteKeys = np.array(['a','s','d','f','g','h','j']);
@@ -205,7 +207,7 @@ root.configure(background='#B22222');
 
 #white keys
 for n in range(numWhiteKeys):
-    w = tk.Button(root, borderwidth = keyBorder, background='white', height = whiteKeyHeight, width=whiteKeyWidth, command = (lambda n=n: testFunction(whiteNotes[n])));
+    w = tk.Button(root, borderwidth = keyBorder, background='white', height = whiteKeyHeight, width=whiteKeyWidth, command = (lambda n=n: simulate(whiteNotes[n])));
 
     #bind a keypress and release to the button
     root.bind(whiteKeys[n], (lambda event, w=w: onPressed(w)));
@@ -219,7 +221,7 @@ currentKey = 0;
 for n in range(numWhiteKeys-1):
     #if there is a black note between the white notes
     if(whiteNotes[n+1] - whiteNotes[n] != 1):
-        w = tk.Button(root, borderwidth = keyBorder, background='black', height = blackKeyHeight, width=blackKeyWidth, command = (lambda n=n: testFunction(whiteNotes[n]+1)));
+        w = tk.Button(root, borderwidth = keyBorder, background='black', height = blackKeyHeight, width=blackKeyWidth, command = (lambda n=n: simulate(whiteNotes[n]+1)));
     
         #bind a keypress and release to the button
         root.bind(blackKeys[currentKey], (lambda event, w=w: onPressed(w)));
